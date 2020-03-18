2b24108798cce70e965cbb848545deb0ebce667f
mar10n <mart103j@edu.campusvejle.dk>
2020-03-18 14:36:05 +0100

    Fixed nav

--- .history/includes/nav_20200318140834.php
+++ .history/includes/nav_20200318140834.php
@@ -1,5 +1,5 @@
 <nav class="customstyle navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#"><img class="img-fluid" src="img/LOGO-Starupuif.png" alt=""></a>
+    <a class="navbar-brand" href="#"><img src="V3.1-eksamen/img/LOGO-Starupuif.png" alt=""></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -9,16 +9,16 @@
                 <a class="nav-link" href="index.php">Forside</a>
             </li>
             <?php
-            if (isset($_SESSION["loggedin"]))
+            /*if (isset($_SESSION["loggedin"]))
             {
                 echo "<li class='nav-item'>" . "<a class='nav-link' href=" . "loggedOut.php" . ">Log ud</a></li>";
-                echo "<li class='nav-item'>" . "<a class='nav-link' href='visBruger.php'>Min konto</a></li>";
+                echo "<li class='nav-item'>" . "<a class='nav-link href=" . "visBruger.php" . ">Min konto</a></li>";
             }
             else 
             {
-                echo "<li class='nav-item'>" . "<a class='nav-link' href='login.php'>Log ind</a></li>";
-                echo "<li class='nav-item'>" . "<a class='nav-link' href='opretBruger.php'>Opret bruger</a></li>";
-            }
+                echo "<li class='nav-item'>" . "<a class='nav-link href=" . "'login.php'>Log ind</a></li>";
+                echo "<li class='nav-item'>" . "<a class='nav-link href=" . "opretBruger.php" . ">Opret bruger</a></li>";
+            }*/
         ?>
         </ul>
     </div>
--- .history/includes/nav_20200318141013.php
+++ .history/includes/nav_20200318141013.php
@@ -1,5 +1,5 @@
 <nav class="customstyle navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#"><img class="img-fluid" src="img/LOGO-Starupuif.png" alt=""></a>
+    <a class="navbar-brand" href="#"><img src="V3.1-eksamen/img/LOGO-Starupuif.png" alt=""></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -12,12 +12,12 @@
             if (isset($_SESSION["loggedin"]))
             {
                 echo "<li class='nav-item'>" . "<a class='nav-link' href=" . "loggedOut.php" . ">Log ud</a></li>";
-                echo "<li class='nav-item'>" . "<a class='nav-link' href='visBruger.php'>Min konto</a></li>";
+                echo "<li class='nav-item'>" . "<a class='nav-link href=" . "visBruger.php" . ">Min konto</a></li>";
             }
             else 
             {
-                echo "<li class='nav-item'>" . "<a class='nav-link' href='login.php'>Log ind</a></li>";
-                echo "<li class='nav-item'>" . "<a class='nav-link' href='opretBruger.php'>Opret bruger</a></li>";
+                echo "<li class='nav-item'>" . "<a class='nav-link href='login.php'>Log ind</a></li>";
+                echo "<li class='nav-item'>" . "<a class='nav-link href='opretBruger.php'>Opret bruger</a></li>";
             }
         ?>
         </ul>
